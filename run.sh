f7b83e5ec7d9f84fa650dd0eced1f3eba764710c
renxiang <renxiang03@meituan.com>
2019-10-27 20:00:11 +0800

    bugfix deploy

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 if [ ! -d /var/run/project ]; then
-    sudo mkdir /var/run/project
-    chowm -R $USER:$USER /var/run/project
+    sudo mkdir /var/run/project && sudo chown -R $USER:$USER /var/run/project
 fi
 gunicorn -c deploy/gunicorn.conf  geekHouse.wsgi
