8a9fd2206e913d3a59e8de04d738ee92c898c3ab
renxiang <renxiang03@meituan.com>
2019-10-27 19:58:14 +0800

    modify deploy

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+if [ ! -d /var/run/project ]; then
+    sudo mkdir /var/run/project
+    chowm -R $USER:$USER /var/run/project
+fi
 gunicorn -c deploy/gunicorn.conf  geekHouse.wsgi
